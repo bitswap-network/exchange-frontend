976dc9c9e145f179f9d06dc00cce2c63d143240a
Tony Zhao <tawnyzhao@gmail.com>
2021-05-14 00:28:59 -0700

    Add types

---
+++ src/interfaces/Pool.ts
@@ -0,0 +1,15 @@
+import { User } from './User'
+
+export interface Pool {
+    _id: string
+    address: string
+    privateKey: {
+        salt: string
+        encryptedKey: string
+    }
+    active: boolean
+    activeStart: number | null
+    user: User | string | null
+    super: number
+    balance: number
+}
