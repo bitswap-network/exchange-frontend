544feac94e1766db0e57ca209460838296ce70d0
ginabak <gina@ginabak.com>
2021-05-15 16:47:10 -0400

    Adding homepage

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,17 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import React from 'react'
 import { NavBar } from '../../components/NavBar'
+import { Box, Flex, Heading, Text, VStack } from '@chakra-ui/react'
 
 export function Home() {
     return (
-        <div>
-            <NavBar loggedOut />
-        </div>
+        <>
+            <NavBar loggedOut={false} />
+            <Flex bg="background.primary">
+                <Box p="20">
+                    <Heading size="lg">Wallet</Heading>
+                </Box>
+            </Flex>
+        </>
     )
 }
