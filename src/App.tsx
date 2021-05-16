a41bac935273bbe89749c5a6584d01da54fd0aba
Alex Yu <alexjy@yahoo.com>
2021-05-15 20:41:41 -0400

    add recoil for state management and basic userState atom

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
 import * as React from 'react'
 import { ChakraProvider, theme } from '@chakra-ui/react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { RecoilRoot } from 'recoil'
 import ChakraHome from './pages/ChakraHome'
 import ChakraDemo from './pages/ChakraDemo'
 
 export const App = (): React.ReactElement => (
-    <ChakraProvider theme={theme}>
-        <BrowserRouter>
-            <Switch>
-                <Route exact path="/demo">
-                    <ChakraDemo />
-                </Route>
-                <Route path="/">
-                    <ChakraHome />
-                </Route>
-            </Switch>
-        </BrowserRouter>
-    </ChakraProvider>
+    <RecoilRoot>
+        <ChakraProvider theme={theme}>
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/demo">
+                        <ChakraDemo />
+                    </Route>
+                    <Route path="/">
+                        <ChakraHome />
+                    </Route>
+                </Switch>
+            </BrowserRouter>
+        </ChakraProvider>
+    </RecoilRoot>
 )
