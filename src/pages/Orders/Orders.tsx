801e7070894b3c4259b2dd164272a3d997467e07
Tony Zhao <tawnyzhao@gmail.com>
2021-05-17 17:47:11 -0700

    Add placeholder pages

---
+++ src/pages/Orders/Orders.tsx
@@ -0,0 +1,10 @@
+import { Box, Heading } from '@chakra-ui/react'
+import React from 'react'
+
+export function Orders(): React.ReactElement {
+    return (
+        <Box>
+            <Heading> Orders Page </Heading>
+        </Box>
+    )
+}
---
+++ src/pages/Wallet/Wallet.tsx
@@ -0,0 +1,10 @@
+import { Box, Heading } from '@chakra-ui/react'
+import React from 'react'
+
+export function Wallet(): React.ReactElement {
+    return (
+        <Box>
+            <Heading> Wallet Page </Heading>
+        </Box>
+    )
+}
