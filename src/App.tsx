36c9e7eef18a393195e801f759ab5b1a67e4362d
Tony Zhao <tawnyzhao@gmail.com>
2021-05-17 17:46:19 -0700

    Add layouting to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,51 @@
 import * as React from 'react'
-import { ChakraProvider } from '@chakra-ui/react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { Box, Container, ChakraProvider, Grid } from '@chakra-ui/react'
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
 import ChakraHome from './pages/ChakraHome'
 import ChakraDemo from './pages/ChakraDemo'
 import { StoryBook } from './pages/StoryBook'
 import { Home } from './pages/Home'
 import { Login } from './pages/Login'
 import { bitswapTheme } from './theme'
+import { NavBar } from './components/NavBar'
+import { Orders } from './pages/Orders/Orders'
+import { Wallet } from './pages/Wallet'
+import { Profile } from './pages/Profile'
 
 export const App = (): React.ReactElement => (
     <ChakraProvider theme={bitswapTheme}>
         <BrowserRouter>
-            <Switch>
-                <Route path="/storybook">
-                    <StoryBook />
-                </Route>
-                <Route exact path="/demo">
-                    <ChakraDemo />
-                </Route>
-                <Route exact path="/chakrahome">
-                    <ChakraHome />
-                </Route>
-                <Route path="/login">
-                    <Login />
-                </Route>
-                <Route path="/">
-                    <Home />
-                </Route>
-            </Switch>
+            <Grid h="100vh" templateRows={'64px 1fr'}>
+                {/* TODO: implement auth to set loggedOut */}
+                <NavBar loggedOut={false} />
+                <Box bg="background.primary" p={8}>
+                    <Container centerContent>
+                        <Switch>
+                            <Route path="/storybook">
+                                <StoryBook />
+                            </Route>
+                            <Route path="/login">
+                                <Login />
+                            </Route>
+                            <Route path="/orders">
+                                <Orders />
+                            </Route>
+                            <Route path="/wallet">
+                                <Wallet />
+                            </Route>
+                            <Route path="/profile">
+                                <Profile />
+                            </Route>
+                            <Route path="/home">
+                                <Redirect to="/" />
+                            </Route>
+                            <Route path="/">
+                                <Home />
+                            </Route>
+                        </Switch>
+                    </Container>
+                </Box>
+            </Grid>
         </BrowserRouter>
     </ChakraProvider>
 )
