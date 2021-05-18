4c07b7e73b54738b2b24917890730089e1d5a008
Tony Zhao <tawnyzhao@gmail.com>
2021-05-17 18:37:08 -0700

    Remove unused import

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import React from 'react'
-import { NavBar } from '../../components/NavBar'
 import { BlueButton } from '../../components/BlueButton/BlueButton'
 import { VStack, Box, Text, Image } from '@chakra-ui/react'
 
