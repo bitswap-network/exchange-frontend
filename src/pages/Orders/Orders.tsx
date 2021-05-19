6f300c84978b399731851340ca00489b29ed517e
Tony Zhao <tawnyzhao@gmail.com>
2021-05-19 03:26:24 -0700

    Add components/themes to Order page

--- src/pages/Orders/Orders.tsx
+++ src/pages/Orders/Orders.tsx
@@ -1,5 +1,4 @@
 import {
-    Box,
     Heading,
     Flex,
     Spacer,
@@ -12,6 +11,7 @@ import {
     Center,
     Stack,
     HStack,
+    useDisclosure,
 } from '@chakra-ui/react'
 import React from 'react'
 import { Column } from 'react-table'
@@ -20,6 +20,7 @@ import { OrderCard } from './OrderCard'
 import { AiOutlineSearch, AiOutlineInfoCircle } from 'react-icons/ai'
 import { Order, OrderType } from '../../types/Order'
 import { OrderTable } from './OrderTable'
+import { OrderModal } from './OrderModal'
 
 export function Orders(): React.ReactElement {
     const data = React.useMemo(
@@ -70,89 +71,94 @@ export function Orders(): React.ReactElement {
         []
     ) as Column<Order>[]
 
+    const { isOpen, onOpen, onClose } = useDisclosure()
+
     return (
-        <VStack spacing={8}>
-            <Flex w="100%">
-                <Heading> Orders </Heading>
-                <Spacer />
-                <BlueButton text="New Order" />
-            </Flex>
-            <Flex w="100%" wrap="wrap">
-                {/* TODO: Replace links and statuses with data here  */}
-                <OrderCard
-                    title="Ongoing Orders"
-                    linkText="Review"
-                    linkSrc="/home"
-                    status="Ordered two minutes ago"
-                    icon={<AiOutlineSearch />}
-                />
-                <Spacer />
-                <OrderCard
-                    title="Pending Orders"
-                    linkText="Learn More"
-                    linkSrc="/home"
-                    status="This order has not been processed"
-                    icon={<AiOutlineInfoCircle />}
-                />
-                <Spacer />
-                <OrderCard
-                    title="Ongoing Orders"
-                    linkText="Learn More"
-                    linkSrc="/home"
-                    status="You can no longer cancel this offer"
-                    icon={<AiOutlineInfoCircle />}
-                />
-            </Flex>
-            <Tabs w="100%" variant="enclosed-colored">
-                <Center>
-                    <TabList>
-                        <Tab>All Orders</Tab>
-                        <Tab>Active Orders</Tab>
-                        <Tab>Pending Orders</Tab>
-                        <Tab>Fulfilled Orders</Tab>
-                    </TabList>
-                </Center>
-                <TabPanels>
-                    <TabPanel>
-                        <Stack spacing={4}>
-                            <Heading as="h2" size="md">
-                                All Orders ({data.length})
-                            </Heading>
-                            <OrderTable data={data} columns={columns} />
-                            <HStack spacing={4} alignItems="flex-start">
-                                <Stack spacing={4} w="100%">
-                                    <Heading as="h2" size="md">
-                                        Buy Orders
-                                    </Heading>
-                                    <OrderTable
-                                        data={data.filter(
-                                            (order) =>
-                                                order.type == OrderType.Buy
-                                        )}
-                                        columns={columns}
-                                    />
-                                </Stack>
-                                <Spacer />
-                                <Stack spacing={4} w="100%">
-                                    <Heading as="h2" size="md">
-                                        Sell Orders
-                                    </Heading>
-                                    <OrderTable
-                                        data={data.filter(
-                                            (order) =>
-                                                order.type == OrderType.Sell
-                                        )}
-                                        columns={columns}
-                                    />
-                                </Stack>
-                            </HStack>
-                        </Stack>
-                    </TabPanel>
-                    <TabPanel>Tab 2</TabPanel>
-                    <TabPanel>Tab 3</TabPanel>
-                    <TabPanel>Tab 4</TabPanel>
-                </TabPanels>
-            </Tabs>
-        </VStack>
+        <>
+            <OrderModal isOpen={isOpen} onClose={onClose} />
+            <VStack spacing={8}>
+                <Flex w="100%">
+                    <Heading> Orders </Heading>
+                    <Spacer />
+                    <BlueButton text="New Order" onClick={onOpen} />
+                </Flex>
+                <Flex w="100%" wrap="wrap">
+                    {/* TODO: Replace links and statuses with data here  */}
+                    <OrderCard
+                        title="Ongoing Orders"
+                        linkText="Review"
+                        linkSrc="/home"
+                        status="Ordered two minutes ago"
+                        icon={<AiOutlineSearch />}
+                    />
+                    <Spacer />
+                    <OrderCard
+                        title="Pending Orders"
+                        linkText="Learn More"
+                        linkSrc="/home"
+                        status="This order has not been processed"
+                        icon={<AiOutlineInfoCircle />}
+                    />
+                    <Spacer />
+                    <OrderCard
+                        title="Ongoing Orders"
+                        linkText="Learn More"
+                        linkSrc="/home"
+                        status="You can no longer cancel this offer"
+                        icon={<AiOutlineInfoCircle />}
+                    />
+                </Flex>
+                <Tabs w="100%" variant="order">
+                    <Center>
+                        <TabList>
+                            <Tab>All Orders</Tab>
+                            <Tab>Active Orders</Tab>
+                            <Tab>Pending Orders</Tab>
+                            <Tab>Fulfilled Orders</Tab>
+                        </TabList>
+                    </Center>
+                    <TabPanels>
+                        <TabPanel>
+                            <Stack spacing={4}>
+                                <Heading as="h2" size="md">
+                                    All Orders ({data.length})
+                                </Heading>
+                                <OrderTable data={data} columns={columns} />
+                                <HStack spacing={4} alignItems="flex-start">
+                                    <Stack spacing={4} w="100%">
+                                        <Heading as="h2" size="md">
+                                            Buy Orders
+                                        </Heading>
+                                        <OrderTable
+                                            data={data.filter(
+                                                (order) =>
+                                                    order.type == OrderType.Buy
+                                            )}
+                                            columns={columns}
+                                        />
+                                    </Stack>
+                                    <Spacer />
+                                    <Stack spacing={4} w="100%">
+                                        <Heading as="h2" size="md">
+                                            Sell Orders
+                                        </Heading>
+                                        <OrderTable
+                                            data={data.filter(
+                                                (order) =>
+                                                    order.type == OrderType.Sell
+                                            )}
+                                            columns={columns}
+                                        />
+                                    </Stack>
+                                </HStack>
+                            </Stack>
+                        </TabPanel>
+                        <TabPanel>Tab 2</TabPanel>
+                        <TabPanel>Tab 3</TabPanel>
+                        <TabPanel>Tab 4</TabPanel>
+                    </TabPanels>
+                </Tabs>
+            </VStack>
+        </>
     )
 }
