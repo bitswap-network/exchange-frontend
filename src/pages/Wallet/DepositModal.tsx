ac5a0363abc8d40b07bccfb854a71e545b0e23c4
Eshwar Chockalingam <eshchock1@gmail.com>
2021-05-25 21:13:49 -0400

    UI for withdraw and deposit modals complete

--- src/pages/Wallet/DepositModal.tsx
+++ src/pages/Wallet/DepositModal.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import {
     Modal,
     Text,
@@ -30,10 +30,10 @@ export const DepositModal: React.FC<ModalProps> = ({
 }: ModalProps) => {
     const user = useRecoilValue(userState)
     const identityUserData = useRecoilValue(identityUsers)
-    const [depositValue, setDepositValue] = useState<string>('0')
+    const [depositValue, setDepositValue] = useState<string>('0.0000')
     const [preflight, setPreflight] =
         useState<TransactionAPIInterface | null>(null)
-
+    const [page, setPage] = useState('deposit')
     const getPreflight = () => {
         bitcloutPreflightTxn(parseFloat(depositValue))
             .then((response) => {
@@ -65,111 +65,192 @@ export const DepositModal: React.FC<ModalProps> = ({
         setDepositValue(valueString.replace(/^\$/, ''))
     }
 
-    // const getMaxBitclout = () => {
-    //     if (user.balance.bitclout > 0) {
-    //         bitcloutPreflightTxn(user.balance.bitclout)
-    //             .then((response) => {
-    //                 setDepositValue(
-    //                     user.balance.bitclout -
-    //                         parseInt(response.data.FeeNanos) / 1e9
-    //                 )
-    //             })
-    //             .catch((error) => {
-    //                 console.log(error)
-    //             })
-    //     }
-    // }
-
-    // user.balance.bitclout
-    const deposit = (
-        <Modal isOpen={disclosure.isOpen} onClose={disclosure.onClose}>
-            <ModalOverlay />
-            <ModalContent>
-                <ModalCloseButton />
-                <ModalBody>
+    const completed = (
+        <ModalContent>
+            <ModalCloseButton />
+            <ModalBody>
+                <Flex w="80%" ml="10%" flexDir="column">
                     <Text
-                        textAlign="center"
-                        fontSize="xx-large"
+                        fontSize="xl"
                         fontWeight="700"
-                        w="full"
                         mt="6"
+                        mb="2"
+                        color="gray.700"
                     >
-                        {user.balance.bitclout} BCLT
+                        Transaction Completed
                     </Text>
-                    <Text
-                        textAlign="center"
-                        color="gray.500"
-                        fontSize="sm"
-                        w="full"
-                        mb="6"
-                    >
-                        Currently Available
+                    <Text color="gray.500" fontSize="sm">
+                        Your transaction has been completed successfully. Click
+                        confirm to return to your wallet.
                     </Text>
-                    <Flex w="80%" ml="10%" flexDir="column">
-                        <Text
-                            fontSize="xl"
-                            fontWeight="700"
-                            mb="2"
-                            color="gray.700"
-                        >
-                            Deposit Funds
-                        </Text>
-                        <Text color="gray.500" fontSize="sm">
-                            Add funds to your BitSwap wallet!
-                        </Text>
-                        <Text
-                            color="gray.600"
-                            fontSize="sm"
-                            fontWeight="600"
-                            mt="6"
-                        >
-                            Amount of BCLT to Deposit
-                        </Text>
-                        <NumberInput
-                            mt="4"
-                            type="text"
-                            placeholder="0.0"
-                            value={depositValue}
-                            onChange={valueHandler}
-                            precision={4}
-                            step={0.1}
-                            min={0}
-                        >
-                            <NumberInputField />
-                            <NumberInputStepper>
-                                <NumberIncrementStepper />
-                                <NumberDecrementStepper />
-                            </NumberInputStepper>
-                        </NumberInput>
-                        <Text color="gray.500" fontSize="sm" mt="6">
-                            You will be able to review this transaction before
-                            it’s complete.
-                        </Text>
-                        <Flex
-                            flexDir="row"
-                            justifyContent="space-between"
-                            w="full"
-                            mt="6%"
-                            mb="8%"
-                        >
-                            <Button
-                                w="47%"
-                                variant="solid"
-                                onClick={disclosure.onClose}
-                            >
-                                Cancel
-                            </Button>
-                            <BlueButton
-                                w="47%"
-                                text={`   Confirm   `}
-                                onClick={disclosure.onClose}
-                            />
-                        </Flex>
-                    </Flex>
-                </ModalBody>
-            </ModalContent>
-        </Modal>
+                    <BlueButton
+                        w="70%"
+                        mt="6"
+                        mb="8"
+                        ml="15%"
+                        text={`   Confirm   `}
+                        onClick={() => {
+                            disclosure.onClose()
+                            setPage('deposit')
+                            setDepositValue('0')
+                        }}
+                    />
+                </Flex>
+            </ModalBody>
+        </ModalContent>
     )
 
-    return deposit
+    const confirmDeposit = (
+        <ModalContent>
+            <ModalCloseButton />
+            <ModalBody>
+                <Flex w="80%" ml="10%" flexDir="column">
+                    <Text
+                        fontSize="xl"
+                        fontWeight="700"
+                        mt="6"
+                        mb="2"
+                        color="gray.700"
+                    >
+                        Confirm Deposit
+                    </Text>
+                    <Text color="gray.500" fontSize="sm">
+                        The following amount will be deposited to your BitSwap
+                        account
+                    </Text>
+                    <Text
+                        color="gray.700"
+                        fontSize="md"
+                        fontWeight="600"
+                        mt="4"
+                        mb="2"
+                    >
+                        Amount of BCLT to Deposit: {depositValue}
+                    </Text>
+                    <Flex
+                        flexDir="row"
+                        justifyContent="space-between"
+                        w="full"
+                        mt="6%"
+                        mb="8%"
+                    >
+                        <Button
+                            w="47%"
+                            variant="solid"
+                            onClick={() => {
+                                setPage('deposit')
+                            }}
+                        >
+                            Modify
+                        </Button>
+                        <BlueButton
+                            w="47%"
+                            text={`   Confirm   `}
+                            onClick={() => {
+                                setPage('completed')
+                            }}
+                        />
+                    </Flex>
+                </Flex>
+            </ModalBody>
+        </ModalContent>
+    )
+
+    const deposit = (
+        <ModalContent>
+            <ModalCloseButton />
+            <ModalBody>
+                <Text
+                    textAlign="center"
+                    fontSize="xx-large"
+                    fontWeight="700"
+                    w="full"
+                    mt="6"
+                >
+                    {user.balance.bitclout} BCLT
+                </Text>
+                <Text
+                    textAlign="center"
+                    color="gray.500"
+                    fontSize="sm"
+                    w="full"
+                    mb="6"
+                >
+                    Currently Available
+                </Text>
+                <Flex w="80%" ml="10%" flexDir="column">
+                    <Text
+                        fontSize="xl"
+                        fontWeight="700"
+                        mb="2"
+                        color="gray.700"
+                    >
+                        Deposit Funds
+                    </Text>
+                    <Text color="gray.500" fontSize="sm">
+                        Add funds to your BitSwap wallet!
+                    </Text>
+                    <Text
+                        color="gray.600"
+                        fontSize="sm"
+                        fontWeight="600"
+                        mt="6"
+                    >
+                        Amount of BCLT to Deposit
+                    </Text>
+                    <NumberInput
+                        mt="4"
+                        type="text"
+                        placeholder="0.0"
+                        value={depositValue}
+                        onChange={valueHandler}
+                        precision={4}
+                        step={0.1}
+                        min={0}
+                    >
+                        <NumberInputField />
+                        <NumberInputStepper>
+                            <NumberIncrementStepper />
+                            <NumberDecrementStepper />
+                        </NumberInputStepper>
+                    </NumberInput>
+                    <Text color="gray.500" fontSize="sm" mt="6">
+                        You will be able to review this transaction before it’s
+                        complete.
+                    </Text>
+                    <Flex
+                        flexDir="row"
+                        justifyContent="space-between"
+                        w="full"
+                        mt="6%"
+                        mb="8%"
+                    >
+                        <Button
+                            w="47%"
+                            variant="solid"
+                            onClick={disclosure.onClose}
+                        >
+                            Cancel
+                        </Button>
+                        <BlueButton
+                            w="47%"
+                            text={`   Confirm   `}
+                            onClick={() => setPage('confirmDeposit')}
+                        />
+                    </Flex>
+                </Flex>
+            </ModalBody>
+        </ModalContent>
+    )
+
+    return (
+        <Modal isOpen={disclosure.isOpen} onClose={disclosure.onClose}>
+            <ModalOverlay />
+            {page == 'deposit'
+                ? deposit
+                : page == 'confirmDeposit'
+                ? confirmDeposit
+                : completed}
+        </Modal>
+    )
 }
--- src/pages/Wallet/WithdrawModal.tsx
+++ src/pages/Wallet/WithdrawModal.tsx
@@ -1,27 +1,25 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import {
-    Box,
-    Flex,
-    Heading,
-    VStack,
-    SimpleGrid,
-    useDisclosure,
     Modal,
+    Text,
     ModalOverlay,
     ModalContent,
-    ModalHeader,
-    ModalFooter,
     ModalBody,
     ModalCloseButton,
+    Flex,
+    Button,
+    NumberInput,
+    NumberInputField,
+    NumberInputStepper,
+    NumberIncrementStepper,
+    NumberDecrementStepper,
 } from '@chakra-ui/react'
-import { BalanceCard } from '../../components/BalanceCard'
-import { CryptoCard } from '../../components/CryptoCard'
-import { Table, Thead, Tbody, Tr, Th, Td } from '@chakra-ui/react'
 import { useRecoilValue } from 'recoil'
-import { userState } from '../../store'
-import { getEthUSD, getBitcloutUSD } from '../../services/utility'
-import { launch, sign } from '../../services/identity'
+import { userState, identityUsers } from '../../store'
+import { bitcloutPreflightTxn } from '../../services/gateway'
+import { handleBitcloutDeposit } from '../../services/identity/helper'
 import { BlueButton } from '../../components/BlueButton'
+import { TransactionAPIInterface } from '../../interfaces/bitclout/Transaction'
 
 interface ModalProps {
     disclosure: any
@@ -30,25 +28,275 @@ interface ModalProps {
 export const WithdrawModal: React.FC<ModalProps> = ({
     disclosure,
 }: ModalProps) => {
+    const user = useRecoilValue(userState)
+    const identityUserData = useRecoilValue(identityUsers)
+    const [withdrawValue, setWithdrawValue] = useState<string>('0.0000')
+    const [preflight, setPreflight] =
+        useState<TransactionAPIInterface | null>(null)
+    const [page, setPage] = useState('withdraw')
+    const getPreflight = () => {
+        bitcloutPreflightTxn(parseFloat(withdrawValue))
+            .then((response) => {
+                setPreflight(response.data.data)
+            })
+            .catch((error) => {
+                console.error(error)
+            })
+    }
+
+    const submitDeposit = () => {
+        if (preflight) {
+            const depositObj = {
+                accessLevel:
+                    identityUserData[user.bitclout.publicKey].accessLevel,
+                accessLevelHmac:
+                    identityUserData[user.bitclout.publicKey].accessLevelHmac,
+                encryptedSeedHex:
+                    identityUserData[user.bitclout.publicKey].encryptedSeedHex,
+                transactionHex: preflight.TransactionHex,
+                transactionIDBase58Check: preflight.TransactionIDBase58Check,
+                value: parseFloat(withdrawValue),
+            }
+            handleBitcloutDeposit(depositObj)
+        }
+    }
+
+    const valueHandler = async (valueString: string) => {
+        const max = (await getMaxBitclout()) as number
+        if (max) {
+            if (parseFloat(valueString) > max) {
+                setWithdrawValue(max.toFixed(4))
+            } else {
+                setWithdrawValue(valueString.replace(/^\$/, ''))
+            }
+        } else {
+            setWithdrawValue('0.0000')
+        }
+    }
+
+    const getMaxBitclout = () => {
+        if (user.balance.bitclout > 0) {
+            bitcloutPreflightTxn(user.balance.bitclout)
+                .then((response) => {
+                    setWithdrawValue(
+                        (
+                            user.balance.bitclout -
+                            parseFloat(response.data.FeeNanos) / 1e9
+                        ).toString()
+                    )
+                    return (
+                        user.balance.bitclout -
+                        parseFloat(response.data.FeeNanos) / 1e9
+                    )
+                })
+                .catch((error) => {
+                    console.log(error)
+                    return 0
+                })
+        } else {
+            setWithdrawValue('0.0000')
+            return 0
+        }
+    }
+
+    // user.balance.bitclout
+
+    const completed = (
+        <ModalContent>
+            <ModalCloseButton />
+            <ModalBody>
+                <Flex w="80%" ml="10%" flexDir="column">
+                    <Text
+                        fontSize="xl"
+                        fontWeight="700"
+                        mt="6"
+                        mb="2"
+                        color="gray.700"
+                    >
+                        Transaction Completed
+                    </Text>
+                    <Text color="gray.500" fontSize="sm">
+                        Your transaction has been completed successfully. Click
+                        confirm to return to your wallet.
+                    </Text>
+                    <BlueButton
+                        w="70%"
+                        mt="6"
+                        mb="8"
+                        ml="15%"
+                        text={`   Confirm   `}
+                        onClick={() => {
+                            disclosure.onClose()
+                            setPage('withdraw')
+                            setWithdrawValue('0')
+                        }}
+                    />
+                </Flex>
+            </ModalBody>
+        </ModalContent>
+    )
+
+    const confirmWithdraw = (
+        <ModalContent>
+            <ModalCloseButton />
+            <ModalBody>
+                <Flex w="80%" ml="10%" flexDir="column">
+                    <Text
+                        fontSize="xl"
+                        fontWeight="700"
+                        mt="6"
+                        mb="2"
+                        color="gray.700"
+                    >
+                        Confirm Withdrawl
+                    </Text>
+                    <Text color="gray.500" fontSize="sm">
+                        The following amount will be withdrawn from your BitSwap
+                        account
+                    </Text>
+                    <Text
+                        color="gray.700"
+                        fontSize="md"
+                        fontWeight="600"
+                        mt="4"
+                        mb="2"
+                    >
+                        Amount of BCLT to Withdraw: {withdrawValue}
+                    </Text>
+                    <Flex
+                        flexDir="row"
+                        justifyContent="space-between"
+                        w="full"
+                        mt="6%"
+                        mb="8%"
+                    >
+                        <Button
+                            w="47%"
+                            variant="solid"
+                            onClick={() => {
+                                setPage('withdraw')
+                            }}
+                        >
+                            Modify
+                        </Button>
+                        <BlueButton
+                            w="47%"
+                            text={`   Confirm   `}
+                            onClick={() => {
+                                setPage('completed')
+                            }}
+                        />
+                    </Flex>
+                </Flex>
+            </ModalBody>
+        </ModalContent>
+    )
+
+    const withdraw = (
+        <ModalContent>
+            <ModalCloseButton />
+            <ModalBody>
+                <Text
+                    textAlign="center"
+                    fontSize="xx-large"
+                    fontWeight="700"
+                    w="full"
+                    mt="6"
+                >
+                    {user.balance.bitclout} BCLT
+                </Text>
+                <Text
+                    textAlign="center"
+                    color="gray.500"
+                    fontSize="sm"
+                    w="full"
+                    mb="6"
+                >
+                    Currently Available
+                </Text>
+                <Flex w="80%" ml="10%" flexDir="column">
+                    <Text
+                        fontSize="xl"
+                        fontWeight="700"
+                        mb="2"
+                        color="gray.700"
+                    >
+                        Withdraw Funds
+                    </Text>
+                    <Text color="gray.500" fontSize="sm">
+                        Withdraw funds from your BitSwap wallet!
+                    </Text>
+                    <Text
+                        color="gray.600"
+                        fontSize="sm"
+                        fontWeight="600"
+                        mt="6"
+                    >
+                        Amount of BCLT to Withdraw{' '}
+                        <Button
+                            variant="solid"
+                            fontSize="sm"
+                            p="3"
+                            h="30px"
+                            ml="2"
+                            onClick={getMaxBitclout}
+                        >
+                            Max
+                        </Button>
+                    </Text>
+                    <NumberInput
+                        mt="4"
+                        type="text"
+                        placeholder="0.0"
+                        value={withdrawValue}
+                        onChange={valueHandler}
+                        precision={4}
+                        step={0.1}
+                        min={0}
+                    >
+                        <NumberInputField />
+                        <NumberInputStepper>
+                            <NumberIncrementStepper />
+                            <NumberDecrementStepper />
+                        </NumberInputStepper>
+                    </NumberInput>
+                    <Text color="gray.500" fontSize="sm" mt="6">
+                        You will be able to review this transaction before it’s
+                        complete.
+                    </Text>
+                    <Flex
+                        flexDir="row"
+                        justifyContent="space-between"
+                        w="full"
+                        mt="6%"
+                        mb="8%"
+                    >
+                        <Button
+                            w="47%"
+                            variant="solid"
+                            onClick={disclosure.onClose}
+                        >
+                            Cancel
+                        </Button>
+                        <BlueButton
+                            w="47%"
+                            text={`   Confirm   `}
+                            onClick={() => setPage('confirmWithdraw')}
+                        />
+                    </Flex>
+                </Flex>
+            </ModalBody>
+        </ModalContent>
+    )
+
     return (
         <Modal isOpen={disclosure.isOpen} onClose={disclosure.onClose}>
             <ModalOverlay />
-            <ModalContent>
-                <ModalHeader>Withdraw</ModalHeader>
-                <ModalCloseButton />
-                <ModalBody>
-                    An email was sent to for verification. Check your spam
-                    folder if you cannot find the email.
-                </ModalBody>
-
-                <ModalFooter>
-                    <BlueButton
-                        text={`   Close   `}
-                        onClick={disclosure.onClose}
-                        mr="3"
-                    />
-                </ModalFooter>
-            </ModalContent>
+            {page == 'withdraw'
+                ? withdraw
+                : page == 'confirmWithdraw'
+                ? confirmWithdraw
+                : completed}
         </Modal>
     )
 }
