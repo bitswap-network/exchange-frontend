41c013b4f6ef84475c8a5d0ed17bf4be4b9bf1b5
Aryan Misra <aryanmisra4@gmail.com>
2021-06-27 02:31:31 -0400

    order box touch ups

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -20,6 +20,7 @@ import {
     Tab,
     TabList,
     TabPanels,
+    Link,
     TabPanel,
     Input,
     InputAddon,
@@ -31,6 +32,7 @@ import {
     NumberDecrementStepper,
     Button
 } from "@chakra-ui/react";
+import {ExternalLinkIcon} from '@chakra-ui/icons'
 import {AiFillInfoCircle} from "react-icons/ai";
 import {useUser} from "../../hooks";
 import {Column} from "react-table";
@@ -77,7 +79,7 @@ export function Home(): React.ReactElement {
                     BitSwap is the easiest and fastest way to buy and sell BitClout. Join the world&apos;s first BitClout exchange and start trading today!
                 </Heading>
 
-                <Box boxShadow="2xl" borderRadius="lg" overflow="hidden" bg="white" p="2" mt="4" borderWidth="2px" borderColor="#DDE2E5">
+                <Box boxShadow="0px 0px 30px 12px rgba(0,0,0,0.07)" borderRadius="lg" overflow="hidden" bg="white" p="2" mt="4" borderWidth="2px" borderColor="#DDE2E5">
                     <HStack justify="space-evenly">
                         <Text>
                             <Image src={globalVars.BITCLOUT_LOGO} htmlWidth="32" style={{display: "inline"}} />{" "}
@@ -89,7 +91,7 @@ export function Home(): React.ReactElement {
                         </Text>
                     </HStack>
                 </Box>
-                <Box boxShadow="2xl" borderRadius="lg" overflow="hidden" bg="white" p="2" mt="4" borderWidth="2px" borderColor="#DDE2E5">
+                <Box boxShadow="0px 0px 30px 12px rgba(0,0,0,0.07)" borderRadius="lg" overflow="hidden" bg="white" p="2" mt="4" borderWidth="2px" borderColor="#DDE2E5">
                     <HStack w="full" justify="space-evenly" mt="2" display={{base: "none", md: "flex"}}>
                         <Flex
                             // bgColor="#dbe6ff"
@@ -143,24 +145,27 @@ export function Home(): React.ReactElement {
                 </Box>
             </Flex>
             <Spacer />
-            <Flex flexDirection="column" w={{base: "100%", md: "45%"}} padding={4} justify="">
-                <Box w="80%" bg="yellow" >
+            <Flex flexDirection="column" w={{base: "100%", md: "45%"}} padding={4} >
+                <Box w="90%" >
                     <Text color="red">this component is under construction</Text>
-                    <VStack w="full" >
-                        <HStack w="full" justify="space-evenly" textAlign="center" fontSize="24" fontWeight="bold">
-                            <Box bg="#FAFAFA" w="full" >
-                                <Text color="#2E6DED">Buy</Text>
+                    <VStack w="full" borderColor="#DDE2E5" borderWidth="2px" borderRadius="6" boxShadow="0px 0px 30px 12px rgba(0,0,0,0.07)" >
+                        <HStack spacing={0} borderRadius="inherit" w="full" justify="space-evenly" textAlign="center" fontSize="20" fontWeight="bold">
+                            <Box borderRadius="inherit" w="full" bg="white" py="4">
+                                <Text color="#2E6DED" fontSize="17">Buy</Text>
                             </Box>
-                            <Box w="full" >
-                                <Text>Sell</Text>
+                            <Box borderRadius="inherit" w="full" bg="#F9FBFF" py="4">
+                                <Text fontSize="17" color="#ACB5BD">Sell</Text>
                             </Box>
                         </HStack>
-                        <Text fontSize="16" fontWeight="bold">
-                            Instantly buy CLOUT at the best price.
-                        </Text>
-                        <VStack p="2" >
-                            <Box p="4" borderWidth="2px" borderColor="#DDE2E5" borderRadius="10" py="2">
-                                <Text color="#81868C" fontSize="20px" fontWeight="regular">From</Text>
+                        <Box w="full" align="center" py="2">
+                            <Text fontSize="13" fontWeight="black">
+                                Instantly buy or sell <span><Link to="http://bitclout.com/" isExternal >CLOUT<ExternalLinkIcon mx="2px" mb="4px" /></Link></span> at the best price.
+                            </Text>
+                        </Box>
+
+                        <Flex flexDir='column' align="center" p="2" overflow="auto" h="max" px="5">
+                            <Box borderWidth="2px" borderColor="rgba(221, 226, 229, 0.5)" borderRadius="10" py="3" px="5" >
+                                <Text color="#81868C" fontSize="18" fontWeight="regular">From</Text>
                                 <InputGroup>
                                     <Input type="number" placeholder="6.90" fontSize="48px" height="68px" borderWidth="0px" pl="2" colorScheme="gray" />
                                     <InputRightElement bg="#F9FBFF" w="max" h="40px" borderRadius="6" borderWidth="1px" borderColor="#F3F3F3" p="1" top="unset" bottom="0px">
@@ -171,11 +176,11 @@ export function Home(): React.ReactElement {
                                     </InputRightElement>
                                 </InputGroup>
                             </Box>
-                            <Box bg="white" borderRadius="5" boxSize="41px" borderWidth="2px" borderColor="#DDE2E5" mt="-4" zIndex="overlay" d="flex">
-                                <MdLoop color="#7E91BA" size="31px" style={{margin: "auto"}} />
+                            <Box mt="-3" mb="-3" bg="white" borderRadius="5" boxSize="38px" borderWidth="2px" borderColor="rgba(221, 226, 229, 0.5)" zIndex="overlay" d="flex">
+                                <MdLoop color="#7E91BA" size="28px" style={{margin: "auto"}} />
                             </Box>
-                            <Box py="2" p="4" borderWidth="2px" borderColor="#DDE2E5" borderRadius="10" mt="-4">
-                                <Text color="#81868C" fontSize="20px" fontWeight="regular">To</Text>
+                            <Box p="4" borderWidth="2px" borderColor="rgba(221, 226, 229, 0.5)" borderRadius="10" >
+                                <Text color="#81868C" fontSize="18" fontWeight="regular">To</Text>
                                 <InputGroup>
                                     <Input type="number" placeholder="420" fontSize="48px" height="68px" borderWidth="0px" pl="2" colorScheme="gray" />
                                     <InputRightElement bg="#F9FBFF" w="max" h="40px" borderRadius="6" borderWidth="1px" borderColor="#F3F3F3" p="1" top="unset" bottom="0px">
@@ -186,20 +191,20 @@ export function Home(): React.ReactElement {
                                     </InputRightElement>
                                 </InputGroup>
                             </Box>
-                        </VStack>
-                        <HStack w="full" justify="space-between" px="3" color="#81868C">
+                        </Flex>
+                        <HStack w="full" justify="space-between" px="6" color="#81868C">
                             <Text fontSize="14px" >
                                 Exchange Rate:
                             </Text>
-                            <Text fontSize="14px" fontWeight="bold">1 ETH ~ 60.969 CLOUT</Text>
+                            <Text fontSize="14px" fontWeight="bold">---------</Text>
                         </HStack>
-                        <HStack w="full" justify="space-between" px="3" color="#81868C">
+                        <HStack w="full" justify="space-between" px="6" color="#81868C">
                             <Text fontSize="14px" >
                                 Est. Fees:
                             </Text>
                             <Text fontSize="14px" fontWeight="bold">0.0069 ETH</Text>
                         </HStack>
-                        <VStack p="2" w="full">
+                        <VStack p="2" px="5" w="full">
                             <BlueButton text={"Swap"} w="full" size="lg" />
                             <Text as={Button} color="#81868C">
                                 Advanced Options
